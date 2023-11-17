34820da4c4c1af0e2efd76645800d7b26e9c74e5
bruno <brunoroman43@gmail.com>
2023-11-17 17:25:37 -0300

    Delete example files

--- src/main/java/ar/edu/unnoba/poo2023/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unnoba.poo2023.service;
-
-import ar.edu.unnoba.poo2023.model.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    public User create(User user);
-}
